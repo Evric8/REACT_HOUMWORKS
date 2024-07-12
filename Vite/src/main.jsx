694be28b9cf5fc2b7afcf7f1520ac1f74e8c6a70
Oleksandr <aleksandr.master88@gmail.com>
2024-07-12 11:42:34 +0300

    add import bootstrap/css

--- Vite/src/main.jsx
+++ Vite/src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
