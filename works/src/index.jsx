b8b718f384f9be0d7fb6f306655c99580a00e67b
Oleksandr <aleksandr.master88@gmail.com>
2024-07-12 11:44:09 +0300

    add import bootstrap/css

--- works/src/index.jsx
+++ works/src/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
