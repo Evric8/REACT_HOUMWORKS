c839a6546e89437bee265feffb369d3f9b32ce3b
Oleksandr <aleksandr.master88@gmail.com>
2024-07-14 18:28:22 +0300

    fix css

--- works/src/index.jsx
+++ works/src/index.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
